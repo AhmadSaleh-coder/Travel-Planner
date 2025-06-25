6eaaff3778de3c4618e69efd794340a92fc62632
NUPCO\aasaleh.vpn <ahmadrocks2011@hotmail.com>
2025-06-25 16:20:03 +0300

    build navBar

---
+++ components/navbar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import Image
+ from 'next/image';
+export default function Navbar() {
+    return (
+    <nav className="bd-white shadow-md py-4 border-b border-gray-200" >
+        <div className="container mx-auto flex justify-between items-center px-6 lg:px-8">
+            <Link href={"/"} className='flex items-center'>
+                <Image src={"/logo.png"} alt='logo' width={50} height={50}/>
+                <span className="text-2xl font-bold text-gray-800">Travel Planner</span>
+            </Link>
+            <div className='flex items-center space-x-4'>
+                <Link href={"/trips"} className='text-slate-900 hover:text-sky-600 transition-colors'>
+                    My Trips
+                </Link>
+                <Link href={"/globe"} className='text-slate-900 hover:text-sky-600 transition-colors'>
+                    Globe
+                </Link>
+                <button className='flex items-center justify-center bg-gray-800 hover:bg-gray-900 text-white p-2 rounded-sm cursor-pointer' >
+                    Sign In<svg className="ml-2" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 640 640"><path d="M319.988 7.973C143.293 7.973 0 151.242 0 327.96c0 141.392 91.678 261.298 218.826 303.63 16.004 2.964 21.886-6.957 21.886-15.414 0-7.63-.319-32.835-.449-59.552-89.032 19.359-107.8-37.772-107.8-37.772-14.552-36.993-35.529-46.831-35.529-46.831-29.032-19.879 2.209-19.442 2.209-19.442 32.126 2.245 49.04 32.954 49.04 32.954 28.56 48.922 74.883 34.76 93.131 26.598 2.882-20.681 11.15-34.807 20.315-42.803-71.08-8.067-145.797-35.516-145.797-158.14 0-34.926 12.52-63.485 32.965-85.88-3.33-8.078-14.291-40.606 3.083-84.674 0 0 26.87-8.61 88.029 32.8 25.512-7.075 52.878-10.642 80.056-10.76 27.2.118 54.614 3.673 80.162 10.76 61.076-41.386 87.922-32.8 87.922-32.8 17.398 44.08 6.485 76.631 3.154 84.675 20.516 22.394 32.93 50.953 32.93 85.879 0 122.907-74.883 149.93-146.117 157.856 11.481 9.921 21.733 29.398 21.733 59.233 0 42.792-.366 77.28-.366 87.804 0 8.516 5.764 18.473 21.992 15.354 127.076-42.354 218.637-162.274 218.637-303.582 0-176.695-143.269-319.988-320-319.988l-.023.107z"/></svg>
+                </button>
+            </div>
+        </div>
+    </nav>
+    );
+}
